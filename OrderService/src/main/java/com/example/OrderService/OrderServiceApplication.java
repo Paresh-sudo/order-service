703ce00066546006b224bde3087e78bf989d22bd
Paresh-sudo <pareshpanchal1742@gmail.com>
2024-03-09 23:37:04 +0530

    Added swagger.

--- OrderService/src/main/java/com/example/OrderService/OrderServiceApplication.java
+++ OrderService/src/main/java/com/example/OrderService/OrderServiceApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
